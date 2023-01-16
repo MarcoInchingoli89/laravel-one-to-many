4efa92226776d52cee0ea54d94d3ab97f373f987
Marco Inchingoli <evilmarkus@hotmail.it>
2023-01-16 17:26:23 +0100

    Add  TypeSeeder and type id foreign key table

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,5 +16,12 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
+        $types = ['Product development', 'Business implementation', 'Physical engineering/construction', 'Research and Development', 'Service development', 'Procurement', 'Regulatory/compliance', 'Other'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->save();
+        }
     }
 }
