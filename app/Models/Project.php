fccbdc4ca7e4e1c68df49071f1468ec6964deb6a
Marco Inchingoli <evilmarkus@hotmail.it>
2023-01-16 17:51:07 +0100

    Add relationships on each model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'cover_image', 'description'];
+    protected $fillable = ['title', 'cover_image', 'type_id', 'description'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
